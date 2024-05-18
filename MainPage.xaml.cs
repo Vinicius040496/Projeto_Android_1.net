c4da92ff0dce05615c39b9e9b6d6843f5035f80b
Vinicius Almeida <167120803+Vinicius040496@users.noreply.github.com>
2024-05-18 18:59:57 -0300

    Projeto Finalizado / Versão final
    Projeto finalizado, utilizando plataforma maui.net para aplicação em Android

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : TabbedPage
+    {
+        string Nome;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btn_Clicked(object sender, EventArgs e)
+        {
+            Nome = await DisplayPromptAsync("Nome", "Digite seu nome", Nome, "Fechar");
+            await DisplayAlert("Aviso!", "Ola, " + Nome, "Fechar");
+        }
+    }
+
+}
